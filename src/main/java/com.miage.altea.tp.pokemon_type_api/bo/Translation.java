4740486f0d995e6d175a8c7b0f5c078b0efb4318
elsa1dasilva <59056006+elsa1dasilva@users.noreply.github.com>
2020-03-02 07:45:14 +0100

    add files

---
+++ src/main/java/com.miage.altea.tp.pokemon_type_api/bo/Translation.java
@@ -0,0 +1,4 @@
+package com.miage.altea.tp.pokemon_type_api.bo;
+
+public class Translation {
+}
---
+++ src/main/java/com.miage.altea.tp.pokemon_type_api/repository/TranslationRepository.java
@@ -0,0 +1,4 @@
+package com.miage.altea.tp.pokemon_type_api.repository;
+
+public class TranslationRepository {
+}
